773af9e5067ce50a19c567e460026079a1242fb4
Chris Neveu <mail@chrisneveu.com>
2014-12-02 19:50:30 -0500

    Reexport types from org.jsoup.nodes.

---
+++ src/main/scala/arrxml/package.scala
@@ -0,0 +1,13 @@
+package object arrxml {
+   type Attribute = org.jsoup.nodes.Attribute
+   type Attributes = org.jsoup.nodes.Attributes
+   type DataNode = org.jsoup.nodes.DataNode
+   type Document = org.jsoup.nodes.Document
+   type DocumentType = org.jsoup.nodes.DocumentType
+   type Element = org.jsoup.nodes.Element
+   type Entities = org.jsoup.nodes.Entities
+   type FormElement = org.jsoup.nodes.FormElement
+   type Node = org.jsoup.nodes.Node
+   type TextNode = org.jsoup.nodes.TextNode
+   type XmlDeclaration = org.jsoup.nodes.XmlDeclaration
+}
