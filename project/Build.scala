5c661144a2b0aaeda9ad8ee1d666c2ea340995df
Chris Neveu <mail@chrisneveu.com>
2014-12-02 19:37:39 -0500

    Add scalaz dependency.

--- project/Build.scala
+++ project/Build.scala
@@ -3,23 +3,26 @@ import Keys._
 
 object BuildSettings {
 
-	val scalaV = "2.11.4"
-	
-	val buildSettings = Defaults.defaultSettings ++ Seq(
-		organization	:= "arrxml",
-		version			 := "0.0.1-SNAPSHOT",
-		scalaVersion	:= scalaV
-	)
+   val scalaV = "2.11.4"
+   
+   val buildSettings = Defaults.defaultSettings ++ Seq(
+      organization := "org.arrxml"
+    , version      := "0.0.1-SNAPSHOT"
+    , scalaVersion := scalaV
+    )
 }
 
 object ArrXmlBuild extends Build {
-	import BuildSettings._
+   import BuildSettings._
 
-	lazy val root: Project = Project(
-		"root",
-		file("."),
-		settings = buildSettings ++ Seq(
-			libraryDependencies ++= Seq(
-				"org.jsoup" % "jsoup" % "1.8.1"))
-	)
+   lazy val root: Project = Project(
+      "root"
+    , file(".")
+    , settings = buildSettings ++ Seq(
+         libraryDependencies ++= Seq(
+            "org.jsoup" % "jsoup" % "1.8.1"
+          , "org.scalaz" %% "scalaz-core" % "7.0.6"
+          )
+       )
+    )
 }
